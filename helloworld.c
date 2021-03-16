2e662e04d33f0778b69878530cbfb20c28ddb232
taj-s <72462948+taj-s@users.noreply.github.com>
2021-03-16 10:37:19 -0700

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+// helloworld.c
+// compile : gcc helloworld.c -o helloworld.o
+#include <stdio.h>
+
+int main(){
+		printf("Helloworld\n");
+		printf("Hola Mundo\n");
+		printf("AAAAAAAAAAAAAAAAAAAAAAAAA\n");
+		return 0;
+}
